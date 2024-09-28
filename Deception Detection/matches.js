a1084c845764414db5c4ccf37760b25789d3b2e4
Shiv Akash <75986286+ShivAkash@users.noreply.github.com>
2024-09-28 22:16:19 +0530

    Update matches.js

--- Deception Detection/matches.js
+++ Deception Detection/matches.js
@@ -2,6 +2,5 @@
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   console.log("Recv. Send response = " + document.title);
   sendResponse({ title: document.title, body: document.body.innerText||document.body.textContent });
-
   return true;
 });
